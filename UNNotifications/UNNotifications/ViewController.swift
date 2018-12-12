5fe02053100dfdfa9dec520d993f5e61d1232ad1
eminkotan <34620199+eminkotan@users.noreply.github.com>
2018-12-12 21:27:40 +0300

    Add files via upload

---
+++ UNNotifications/UNNotifications/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  UNNotifications
+//
+//  Created by Emin Kotan on 12.12.2018.
+//  Copyright © 2018 Emin Kotan. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+class ViewController: UIViewController {
+
+     let center = UNUserNotificationCenter.current()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func triggerButton(_ sender: Any) {
+        createNotfications()
+    }
+    
+    func createNotfications() {
+        // CONTENT
+        let content = UNMutableNotificationContent() // Notifications'ı tanımla
+        content.title = "Bildirimin başlığı" // bildirimin başlığı
+        content.subtitle = "Bildirimin altbaşlığı" // bildirimin altbaşlığı
+        content.body = "Bildirimin içeriği" // bildirimin içeriği
+        content.sound = UNNotificationSound.default // bildirimin sesi
+        
+        // TRIGGER
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+       
+        // CUSTOM ACTIONS
+        
+        // Define Action
+        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
+        let deleteAction = UNNotificationAction(identifier: "DeleteAction", title: "Delete", options: [.destructive])
+        
+        // Create Category
+        let category = UNNotificationCategory(identifier: "MyNotificationsCategory", actions: [snoozeAction, deleteAction], intentIdentifiers: [], options: [])
+        
+        // Register Category
+        center.setNotificationCategories([category])
+        content.categoryIdentifier = "MyNotificationsCategory"
+        
+        // REQUEST
+         let identifier = "FirstUserNotification"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            if error != nil {
+                print("Something wrong")
+            }
+        }
+    }
+
+}
+
